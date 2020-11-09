99e9b92335717ece3bb10ce3a566ff4784ab377c
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2020-11-09 00:23:41 -0500

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/manufacturers', to: 'manufacturers#index'
   resources :manufacturers
   get '/products', to: 'products#index'
-  resources :products
+  resources :products 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
