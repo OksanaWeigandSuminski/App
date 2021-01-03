54c690bb06afef2b270f377343783b633eade9ee
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-01-03 16:56:22 -0500

    get instead of root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  get 'home#index'
   get '/security_and_privacy_vulnerabilities', to: 'security_and_privacy_vulnerabilities#index'
   resources :security_and_privacy_vulnerabilities
   get '/manufacturers', to: 'manufacturers#index'
